e9f16ce3266a2b690242481da9bb70f4d12a2a6b
rioxraman <ramanpkgupta@gmail.com>
2024-01-26 23:34:42 +0530

    test 1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'UGC Net Syllabus for Computer Science Option'
+copyright = '2023, Ramn Gupta'
+author = 'Raman Gupta'
 
 release = '0.1'
 version = '0.1.0'
