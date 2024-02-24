8e742514234211cb25bff0866c133ccbabba7a92
rioxraman <ramanpkgupta@gmail.com>
2024-02-24 15:14:55 +0530

    second commit test

--- docs/source/socialbuttons.py
+++ docs/source/socialbuttons.py
@@ -6,4 +6,4 @@ def setup(app: Sphinx):
     app.connect('builder-inited', lambda app: app.config.html_static_path.append('_static'))
 
     # Optional: Include FontAwesome CDN if not already included
-    app.add_js_file('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js', priority=200)
+    app.add_js_file('https://kit.fontawesome.com/f8fd821bc4.js', priority=200)
